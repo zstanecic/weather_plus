4182f3c1fca35645bacb68aa05c03fa439d23a66
abdel792 <abdelkrim.bensaid@gmail.com>
2019-06-17 08:41:39 +0200

    Updated Weather_Plus version to 6.6.

--- buildVars.py
+++ buildVars.py
@@ -30,7 +30,7 @@ addon_info = {
 	_("For other instructions, you can also press Add-on Help button in Add-ons manager."),
 	],
 	# version
-	"addon_version" : "6.5",
+	"addon_version" : "6.6",
 	# Author(s)
 	"addon_author" : "Adriano Barbieri <adrianobarb@yahoo.it>",
 	# URL for the add-on documentation support
