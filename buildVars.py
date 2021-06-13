c62949e74ce08e7e80629a0977995a90a8e67f4b
Rémy Ruiz <remyruiz@gmail.com>
2021-06-13 16:21:00 +0200

    Updated readme.md and buildVars.py for Weather_Plus8.0

--- buildVars.py
+++ buildVars.py
@@ -20,9 +20,10 @@ addon_info = {
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : [
-	_("Adds local temperature and forecast 24 ours and prediction up to 9 days.\n"),
+	_("Adds local temperature and forecast 24 ours and prediction up to 2 days.\n"),
 	_("NVDA+w for the current bulletin.\n"),
-	_("NVDA+shift+w for the prediction in the current 24 hours and prediction up to 9 days.\n"),
+	_("NVDA+shift+w for the prediction in the current 24 hours and prediction up to 2 days.\n"),
+	_("NVDA+shift+w, pressed twice provides the hourlyforecast of the weather conditions.\n"),
 	_("NVDA+shift+control+w for quickly enter a temporary city.\n"),
 	_("NVDA + shift + control + alt + w for open the Weather Plus settings.\n"),
 	_("NVDA+alt+w announces the date of the last update of the weather report.\n"),
@@ -30,7 +31,7 @@ addon_info = {
 	_("For other instructions, you can also press Add-on Help button in Add-ons manager."),
 	],
 	# version
-	"addon_version" : "7.5",
+	"addon_version" : "8.0",
 	# Author(s)
 	"addon_author" : "Adriano Barbieri <adrianobarb@yahoo.it>",
 	# URL for the add-on documentation support
@@ -40,7 +41,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2012.2.1")
 	"addon_minimumNVDAVersion" : "2017.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2019.3.0",
+	"addon_lastTestedNVDAVersion" : "2021.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
